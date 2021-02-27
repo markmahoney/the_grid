38a1a08cade488429b3d6500180b888747ad029b
Mark Mahoney <mmahoney@slack-corp.com>
2021-02-26 16:54:20 -0800

    update conversion script to run from a google spreadsheet ID

--- grid.py
+++ grid.py
@@ -1,9 +1,10 @@
 """
-Dump lookup tables of all Legendary weapons and perks to CSVs for
-importing into Google sheets.
+Turn a The Grid spreadsheet from Google Sheets into a tsv that DIM understands.
 
-The Destiny 2 API is self-referential madness and undocumented. Spelunking
-is best done through https://data.destinysets.com
+Dumps a The Grid from Google Sheets and tries to match weapon and perk rolls
+to values from the D2 API. Once it matches the weapons and perks to the API,
+it dumps out a list of dimwishlist rows as a TSV that can be imported into
+DIM.
 """
 
 import os
@@ -13,28 +14,42 @@ import json
 import tempfile
 import requests
 
-from typing import Dict, Tuple, Set
+from typing import Dict, Tuple, Set, List
 from urllib.parse import urljoin
 
 
-def _fetch_json(url: str, api_key: str) -> Tuple[int, Dict]:
-    resp = requests.get(url, headers={"X-API-Key": api_key})
-
-    try:
-        body_json = resp.json()
-    except ValueError:
-        body_json = None
-
-    return resp.status_code, body_json
-
-
-def _fetch_manifest(api_key: str) -> Dict:
-    code, resp = _fetch_json(
-        "https://www.bungie.net/platform/Destiny2/Manifest/", api_key
+def _normalize_name(name: str) -> str:
+    return " ".join(
+        "".join(c for c in token if c.isalnum()) for token in name.lower().split()
     )
 
-    if code != 200 or resp["ErrorStatus"] != "Success":
-        raise ValueError(f"error fetching manifest: {resp['Message']}")
+
+def _fetch_grid(sheet_id: str) -> List[Tuple[str, str, str]]:
+    csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv"
+    resp = requests.get(csv_url)
+    resp.raise_for_status()
+
+    reader = csv.DictReader(
+        line.decode(resp.apparent_encoding) for line in resp.iter_lines()
+    )
+
+    return [
+        tuple(_normalize_name(row[col]) for col in ["Name", "Perk 1", "Perk 2"])
+        for row in reader
+    ]
+
+
+def _fetch_json(url: str, api_key: str = "") -> Dict:
+    resp = requests.get(url, headers={"X-API-Key": api_key})
+    resp.raise_for_status()
+    return resp.json()
+
+
+def _fetch_manifest(api_key: str = "") -> Dict:
+    resp = _fetch_json("https://www.bungie.net/platform/Destiny2/Manifest/", api_key)
+
+    if resp["ErrorStatus"] != "Success":
+        raise ValueError(f"Bungie API error: {resp['Message']}")
 
     return resp["Response"]
 
@@ -42,10 +57,7 @@ def _fetch_manifest(api_key: str) -> Dict:
 def _fetch_content(manifest: Dict, key: str, api_key: str = "") -> Dict:
     content_url_path = manifest["jsonWorldComponentContentPaths"]["en"][key]
     content_url = urljoin("https://bungie.net", content_url_path)
-    code, data = _fetch_json(content_url, api_key)
-
-    if code != 200:
-        raise ValueError(f"error fetching content blob: {resp.status_code}")
+    data = _fetch_json(content_url, api_key)
 
     return data
 
@@ -59,7 +71,7 @@ def _weapon_names_and_hashes(categories: Dict, items: Dict) -> Dict[int, str]:
     )
 
     return {
-        k: v["displayProperties"]["name"]
+        _normalize_name(v["displayProperties"]["name"]): k
         for (k, v) in items.items()
         if weapon_hash in v.get("itemCategoryHashes", [])
     }
@@ -85,7 +97,7 @@ def _random_roll_perk_ids(item_id: int, items: Dict, plug_sets: Dict) -> Set[int
 
 def _all_random_roll_perks(
     categories: Dict, items: Dict, plug_sets: Dict
-) -> Dict[int, str]:
+) -> Dict[str, int]:
     """
     Dump a map from perk hash to perk name by collecting all possible rolls
     on legendary items.
@@ -105,15 +117,19 @@ def _all_random_roll_perks(
         perk_ids = perk_ids.union(_random_roll_perk_ids(item["hash"], items, plug_sets))
 
     return {
-        perk_id: items[perk_id]["displayProperties"]["name"] for perk_id in perk_ids
+        _normalize_name(items[perk_id]["displayProperties"]["name"]): perk_id
+        for perk_id in perk_ids
     }
 
 
+def _wishlist_url(weapon_hash: int, perk_hashes: List[int], comment: str) -> str:
+    perks = ",".join(str(p) for p in perk_hashes)
+    return f"dimwishlist:item={weapon_hash}&perks={perks}#notes: {comment}"
+
+
 def main():
-    api_key = os.getenv("BUNGIE_API_KEY")
-    if not api_key:
-        print("error: can't find bungie api key")
-        sys.exit(-1)
+    grid_key = "1fPE6BTWjTZlfNOGp6kPOrT6ZqUMasbgHQzJanyHnz48"
+    grid_rolls = _fetch_grid(grid_key)
 
     # pull a bunch of JSON blobs from the D2 API
     #
@@ -123,31 +139,43 @@ def main():
     #
     # once we have the manifest we can pull categories and items to get a list
     # of all weapons and their API hashes.
-    manifest = _fetch_manifest(api_key)
+    manifest = _fetch_manifest()
 
     items = _fetch_content(manifest, "DestinyInventoryItemDefinition")
     categories = _fetch_content(manifest, "DestinyItemCategoryDefinition")
     plug_sets = _fetch_content(manifest, "DestinyPlugSetDefinition")
 
-    weapon_names = sorted(
-        _weapon_names_and_hashes(categories, items).items(), key=lambda x: x[1]
-    )
-    with open("weapon_names.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["name", "hash"])
+    weapons_by_name = _weapon_names_and_hashes(categories, items)
+    perks_by_name = _all_random_roll_perks(categories, items, plug_sets)
 
-        for h, n in weapon_names:
-            writer.writerow([n, h])
+    with open("./the_grid.tsv", "w") as f:
+        print("title:the grid", file=f)
+        print("description: it's the grid baby", file=f)
+        for weapon, p1, p2 in grid_rolls:
+            item_hash = weapons_by_name.get(weapon)
 
-    perk_names = sorted(
-        _all_random_roll_perks(categories, items, plug_sets).items(), key=lambda x: x[1]
-    )
-    with open("perk_names.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["name", "hash"])
+            if not item_hash:
+                print(f"warning: skipping weapon: missing weapon: {weapon}")
+                continue
 
-        for h, n in perk_names:
-            writer.writerow([n, h])
+            p1_hash = perks_by_name.get(p1)
+            if not p1_hash:
+                print(f"warning: skipping weapon: missing perk: {p1}")
+                continue
+
+            p2_hash = perks_by_name.get(p2)
+            if not p2_hash:
+                print(f"warning: skipping weapon: missing perk: {p2}")
+                continue
+
+            print(
+                _wishlist_url(
+                    item_hash,
+                    [p1_hash, p2_hash],
+                    f"the grid (season 13) - {weapon}, {p1}, {p2}",
+                ),
+                file=f,
+            )
 
 
 if __name__ == "__main__":
